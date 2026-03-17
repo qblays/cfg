44865e8793de53a80fa7d4f07d20fbd1ccc7429c
Mikhail Bobrovnikov <mikhail.bobrovnikov@rfdyn.ru>
2026-03-17 12:57:11 +0300

    nvim and tmux

--- neovim/.config/nvim/lua/plugins/my.lua
+++ neovim/.config/nvim/lua/plugins/my.lua
@@ -118,7 +118,7 @@ return {
         },
         line_num = {
           enable = true,
-          use_treesitter = true,
+          use_treesitter = false,
           -- Your undo dump also contained a style rainbow list at some point:
           -- style = {
           --   "#FF0000", "#FF7F00", "#FFFF00", "#00FF00",
@@ -173,5 +173,18 @@ return {
         desc = "FFFind files",
       },
     },
+  },
+  {
+    "MagicDuck/grug-far.nvim",
+    -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+    -- additional lazy config to defer loading is not really needed...
+    config = function()
+      -- optional setup call to override plugin options
+      -- alternatively you can set options with vim.g.grug_far = { ... }
+      require("grug-far").setup({
+        -- options, see Configuration section below
+        -- there are no required options atm
+      })
+    end,
   },
 }
